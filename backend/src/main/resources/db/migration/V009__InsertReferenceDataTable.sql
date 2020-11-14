46e9531727560f9e268e68588ccb8bcc4a741d2b
RD <rory.c.dickinson@gmail.com>
2020-11-14 03:16:12 -0600

    more data

--- backend/src/main/resources/db/migration/V009__InsertReferenceDataTable.sql
+++ backend/src/main/resources/db/migration/V009__InsertReferenceDataTable.sql
@@ -1,6 +1,7 @@
 INSERT INTO reference_data (id,ref_type,ref_value) VALUES
 (nextval('reference_data_id_seq'),'ADJ','Angelic'),
 (nextval('reference_data_id_seq'),'ADJ','Rising'),
+(nextval('reference_data_id_seq'),'ADJ','Snarky'),
 (nextval('reference_data_id_seq'),'ADJ','Lonely'),
 (nextval('reference_data_id_seq'),'ADJ','Deadly'),
 (nextval('reference_data_id_seq'),'ADJ','Wistful'),
@@ -9,12 +10,14 @@ INSERT INTO reference_data (id,ref_type,ref_value) VALUES
 (nextval('reference_data_id_seq'),'ADJ','Sorrowful'),
 (nextval('reference_data_id_seq'),'ADJ','Perfect'),
 (nextval('reference_data_id_seq'),'ADJ','Charming'),
+(nextval('reference_data_id_seq'),'ADJ','Newfound'),
 (nextval('reference_data_id_seq'),'ADJ','Strawberry Death'),
 (nextval('reference_data_id_seq'),'ADJ','Black'),
 (nextval('reference_data_id_seq'),'ADJ','Blue'),
 (nextval('reference_data_id_seq'),'ADJ','Gray'),
 (nextval('reference_data_id_seq'),'ADJ','Green'),
 (nextval('reference_data_id_seq'),'ADJ','White'),
+(nextval('reference_data_id_seq'),'ADJ','Yellow'),
 (nextval('reference_data_id_seq'),'ADJ','Red');
 
 INSERT INTO reference_data (id,ref_type,ref_value) VALUES
@@ -22,6 +25,7 @@ INSERT INTO reference_data (id,ref_type,ref_value) VALUES
 (nextval('reference_data_id_seq'),'NOUN','Napkins'),
 (nextval('reference_data_id_seq'),'NOUN','Beagles'),
 (nextval('reference_data_id_seq'),'NOUN','Bodies'),
+(nextval('reference_data_id_seq'),'NOUN','Gorillas'),
 (nextval('reference_data_id_seq'),'NOUN','Puppies'),
 (nextval('reference_data_id_seq'),'NOUN','Chipmunks'),
 (nextval('reference_data_id_seq'),'NOUN','Socks'),
@@ -29,10 +33,13 @@ INSERT INTO reference_data (id,ref_type,ref_value) VALUES
 (nextval('reference_data_id_seq'),'NOUN','Houseplants'),
 (nextval('reference_data_id_seq'),'NOUN','Mirrors'),
 (nextval('reference_data_id_seq'),'NOUN','Starship'),
+(nextval('reference_data_id_seq'),'NOUN','Barracudas'),
 (nextval('reference_data_id_seq'),'NOUN','Truckers'),
 (nextval('reference_data_id_seq'),'NOUN','Hot Sauces'),
 (nextval('reference_data_id_seq'),'NOUN','Clock'),
 (nextval('reference_data_id_seq'),'NOUN','Snakes'),
+(nextval('reference_data_id_seq'),'NOUN','Weathermen'),
+(nextval('reference_data_id_seq'),'NOUN','Wizards'),
 (nextval('reference_data_id_seq'),'NOUN','Cakes');
 
 INSERT INTO reference_data (id,ref_type,ref_value) VALUES
